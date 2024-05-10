4ee3aa087f94a44cccb3468205fa12e6918fe6bd
fps_netodf <fps_netodf@hotmail.com>
2024-05-10 15:45:30 -0300

    atualizando ate o 33

--- authenticacao/views.py
+++ authenticacao/views.py
@@ -18,12 +18,13 @@ def home(request):
         return render(request, 'authenticacao/home.html', {'nome': nome, 'email': email, 'senha': senha})
 
 def listar(request):
+    pessoas = Pessoa.objects.all()
     # dados = Pessoa.objects.filter(senha = "Paulo1234")[0]
     # adm = Cargo.objects.filter(id=1)[0]
     # dados = Pessoa.objects.filter(nome__startswith="Bruno")[0]
     # dados.cargo_id = adm
     # dados.save() 
-    pessoas = Pessoa.objects.filter(cargo__pk = 2).filter(senha = "rcs1234") | Pessoa.objects.filter(nome__icontains = "alanna")
+    # pessoas = Pessoa.objects.filter(cargo__pk = 2).filter(senha = "rcs1234") | Pessoa.objects.filter(nome__icontains = "alanna")
     # pessoas = Pessoa.objects.filter(nome__startswith='Francisco')| Pessoa.objects.filter(email='bl@gmail.com')
     # pessoas.delete()
     return render (request, 'authenticacao/listar.html', {'pessoas': pessoas})
